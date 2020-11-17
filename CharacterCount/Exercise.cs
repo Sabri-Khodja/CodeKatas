cae39e4d5f22233788b850e260ca998554690f4a
Sabri Khodja <sabri.khodja@gmail.com>
2020-11-17 20:38:31 +0100

    CharacterCount initial commit

---
+++ CharacterCount/Exercise.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections.Generic;
+
+namespace CharacterCount
+{
+    public static class Exercise
+    {
+        // TODO: fix this method - remove boxing & unboxing, and return correct result
+        public static object GetCharacterCount(string name)
+        {
+            var result = new Dictionary<object, object>();
+            foreach (char c in name)
+            {
+                result[c] = 1;
+            }
+            return result;
+        }
+
+        public static Dictionary<char, int> GetCharacterCountFixed(string name)
+        {
+            var result = new Dictionary<char, int>();
+            name = name.ToLower();
+            foreach (char c in name)
+            {
+                if (c != ' ')
+                {
+                    if (result.ContainsKey(c))
+                    {
+                        result[c]++;
+                    }
+                    else
+                    {
+                        result.Add(c, 1);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ CharacterCount/ExerciseUnitTests.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace CharacterCount
+{
+    [TestFixture]
+    public class ExerciseUnitTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+
+        private string _acceptanceTestString = "John Doe";
+
+        private Dictionary<char, int> _acceptanceTestResult = new Dictionary<char, int>()
+        {
+            {'j', 1},
+            {'o', 2},
+            {'h', 1},
+            {'n', 1},
+            {'d', 1},
+            {'e', 1},
+        };
+
+
+        [Test]
+        public void AssessInitialImplementationTest()
+        {
+            var resultAsObject = Exercise.GetCharacterCount(_acceptanceTestString);
+
+            var countPerCharacter = resultAsObject as Dictionary<object, object>;
+
+            //The following assert proves that the initial implementation is defective
+            Assert.AreNotEqual(_acceptanceTestResult.Keys.Count, countPerCharacter.Keys.Count);
+        }
+
+        //1st iteration
+        [Test]
+        public void ReturnsEmptyDictionaryWhenEmptyStringTest()
+        {
+            var countPerCharacter = Exercise.GetCharacterCountFixed(String.Empty);
+
+            Assert.AreEqual(0, countPerCharacter.Keys.Count);
+        }
+
+        //2nd iteration
+        [Test]
+        public void Returns1ForAOneCharStringTest()
+        {
+            var countPerCharacter = Exercise.GetCharacterCountFixed("A");
+
+            Assert.AreEqual(1, countPerCharacter.Keys.Count);
+            Assert.AreEqual(1, countPerCharacter['a']);
+        }
+
+        //3rd iteration
+        [Test]
+        public void AcceptanceTest()
+        {
+            var countPerCharacter = Exercise.GetCharacterCountFixed(_acceptanceTestString);
+
+            Assert.AreEqual(_acceptanceTestResult.Keys.Count, countPerCharacter.Keys.Count);
+
+            Assert.AreEqual(_acceptanceTestResult['j'], countPerCharacter['j']);
+            Assert.AreEqual(_acceptanceTestResult['o'], countPerCharacter['o']);
+            Assert.AreEqual(_acceptanceTestResult['h'], countPerCharacter['h']);
+            Assert.AreEqual(_acceptanceTestResult['n'], countPerCharacter['n']);
+            Assert.AreEqual(_acceptanceTestResult['d'], countPerCharacter['d']);
+            Assert.AreEqual(_acceptanceTestResult['e'], countPerCharacter['e']);
+        }
+
+    }
+}
