f4f9de49eba1e2a7c88afac0c70b14984e5f70fe
Sabri Khodja <sabri.khodja@gmail.com>
2020-11-17 23:09:58 +0100

    MatrixMultiplication initial commit

---
+++ MatrixMultiplication/Exercice.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+
+namespace MatrixMultiplication
+{
+    // TODO: optimize this class
+    //multi dimensional array implementation
+    public class Matrix : IEnumerable
+    {
+        private int[,] _data;
+
+        public int Rows { get { return _data.GetLength(0); } }
+        public int Columns { get { return _data.GetLength(1); } }
+
+        public int this[int row, int col]
+        {
+            get { return _data[row, col]; }
+            set { _data[row, col] = value; }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        public Matrix(int[,] value)
+        {
+            _data = value;
+        }
+    }
+
+    //Jaded array implementation
+    public class MatrixOptimized1 : IEnumerable
+    {
+        private int[][] _data;
+
+        public int Rows { get { return _data.GetLength(0); } }
+        public int Columns { get { return _data[0].GetLength(0); } }
+
+        public int this[int row, int col]
+        {
+            get { return _data[row][col]; }
+            set { _data[row][col] = value; }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        public MatrixOptimized1(int[][] value)
+        {
+            _data = value;
+        }
+    }
+
+    //Flattened array implementation
+    public class MatrixOptimized2 : IEnumerable
+    {
+        private int[] _data;
+
+        public int Rows { get; private set; }
+        public int Columns { get; private set; }
+
+        public int this[int row, int col]
+        {
+            get { return _data[row * Columns + col]; }
+            set { _data[row * Columns + col] = value; }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        public MatrixOptimized2(int[] value, int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            _data = value;
+        }
+    }
+
+    public class Exercise
+    {
+        public static Matrix Multiply(Matrix a, Matrix b)
+        {
+            var result = new Matrix(new int[a.Rows, b.Columns]);
+            for (int i = 0; i < result.Rows; i++)
+            {
+                for (int j = 0; j < result.Columns; j++)
+                {
+                    result[i, j] = 0;
+                    for (int k = 0; k < a.Columns; k++)
+                        result[i, j] += (a[i, k] * b[k, j]);
+                }
+            }
+            return result;
+        }
+
+        public static MatrixOptimized1 Multiply(MatrixOptimized1 a, MatrixOptimized1 b)
+        {
+            var arrayResult = new int[a.Rows][];
+            for (int i = 0; i < a.Rows; i++)
+            {
+                arrayResult[i] = new int[b.Columns];
+            }
+
+            var result = new MatrixOptimized1(arrayResult);
+            for (int i = 0; i < result.Rows; i++)
+            {
+                for (int j = 0; j < result.Columns; j++)
+                {
+                    result[i, j] = 0;
+                    for (int k = 0; k < a.Columns; k++)
+                        result[i, j] += (a[i, k] * b[k, j]);
+                }
+            }
+            return result;
+        }
+
+        public static MatrixOptimized2 Multiply(MatrixOptimized2 a, MatrixOptimized2 b)
+        {
+            var result = new MatrixOptimized2(new int[a.Rows * b.Columns], a.Rows, b.Columns);
+            for (int i = 0; i < result.Rows; i++)
+            {
+                for (int j = 0; j < result.Columns; j++)
+                {
+                    result[i, j] = 0;
+                    for (int k = 0; k < a.Columns; k++)
+                        result[i, j] += (a[i, k] * b[k, j]);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ MatrixMultiplication/ExerciseUnitTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace MatrixMultiplication
+{
+    [TestFixture]
+    public class ExerciseUnitTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            GC.Collect();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            
+        }
+
+
+        [Test]
+        public void AssessInitialCodeTest()
+        {
+            var matrix1 = new Matrix(new int[2,3] { { 1, 2, 3 },  { 4, 5, 6 } });
+            var matrix2 = new Matrix(new int[3,2] { { 7, 8 }, { 9, 10 }, { 11, 12 } });
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Matrix res = null;
+
+            for (int i = 0; i < 50000000; i++)
+            {
+                res = Exercise.Multiply(matrix1, matrix2);
+            }
+
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+
+            Assert.AreEqual(58, res[0, 0]);
+            Assert.AreEqual(64, res[0, 1]);
+            Assert.AreEqual(139, res[1, 0]);
+            Assert.AreEqual(154, res[1, 1]);
+        }
+
+
+        [Test]
+        public void AcceptanceTestImplementation1()
+        {
+            var matrix1 = new MatrixOptimized1(new int[2][] { new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 } } );
+            var matrix2 = new MatrixOptimized1(new int[3][] { new int[] { 7, 8 }, new int[] { 9, 10 }, new int[] { 11, 12 } });
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            MatrixOptimized1 res = null;
+
+            for (int i = 0; i < 50000000; i++)
+            {
+                res = Exercise.Multiply(matrix1, matrix2);
+            }
+
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+
+            Assert.AreEqual(58, res[0, 0]);
+            Assert.AreEqual(64, res[0, 1]);
+            Assert.AreEqual(139, res[1, 0]);
+            Assert.AreEqual(154, res[1, 1]);
+        }
+
+        [Test]
+        public void AcceptanceTestImplementation2()
+        {
+            var matrix1 = new MatrixOptimized2(new int[] { 1, 2, 3, 4, 5, 6 }, 2, 3);
+            var matrix2 = new MatrixOptimized2(new int[] { 7, 8,  9, 10, 11, 12 }, 3, 2);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            MatrixOptimized2 res = null;
+
+            for (int i = 0; i < 50000000; i++)
+            {
+                res = Exercise.Multiply(matrix1, matrix2);
+            }
+
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+
+            Assert.AreEqual(58, res[0, 0]);
+            Assert.AreEqual(64, res[0, 1]);
+            Assert.AreEqual(139, res[1, 0]);
+            Assert.AreEqual(154, res[1, 1]);
+        }
+    }
+}
