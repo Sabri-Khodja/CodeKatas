f184d2787baac17ecf4a1a074c092d21a64c5487
Sabri Khodja <sabri.khodja@gmail.com>
2020-11-17 10:54:52 +0100

    WordWrap initial commit

---
+++ WordWrap/Wrapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordWrap
+{
+    public static class Wrapper
+    {
+        public static string Wrap(string text, int column)
+        {
+            if (text.Length <= column)
+            {
+                return text;
+            }
+
+            StringBuilder formattedTextBuilder = new StringBuilder();
+            var startSearchIndex = column - 1;
+            var startSubstring = 0;
+            var lenghtSubstring = 0;
+            while (startSearchIndex < text.Length)
+            {
+                var indexOfLastSpace = text.LastIndexOf(" ", startSearchIndex, column);
+
+                lenghtSubstring = indexOfLastSpace - ( startSearchIndex - column + 1);
+                formattedTextBuilder.AppendLine(text.Substring(startSubstring, lenghtSubstring));
+                startSubstring = indexOfLastSpace + 1;
+                startSearchIndex = startSubstring + column - 1;
+            }
+
+            formattedTextBuilder.Append(text.Substring(startSubstring, text.Length - startSubstring));
+
+            return formattedTextBuilder.ToString();
+        }
+
+    }
+}
---
+++ WordWrap/WrapperUnitTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+
+namespace WordWrap
+{
+    [TestFixture]
+    public class WrapperUnitTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+        
+        //Iteration 1
+        [Test]
+        public void WrapReturnsSameTextWhenTextLenghtLowerThanColumn()
+        {
+            var text = "The quick brown fox jumps over a lazy dog.";
+            var formattedText = Wrapper.Wrap(text, 80);
+
+            Assert.AreEqual(text, formattedText);
+        }
+
+        //Iteration 2
+        [Test]
+        public void WrapReturns2LinesWhenTextLenghtSlightlyBiggerThanColumn()
+        {
+            var text = "The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog.";
+            var formattedText = Wrapper.Wrap(text, 80);
+
+            var lines = formattedText.Split(new []{ '\n' });
+
+            Assert.AreEqual(2, lines.Length);
+        }
+
+        //Iteration 3
+        [Test]
+        public void WrapReturnsCorrectFormattedTextForMultilineInputText()
+        {
+            var text = "The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog.";
+            var formattedText = Wrapper.Wrap(text, 80);
+            
+            Assert.AreEqual(@"The quick brown fox jumps over a lazy dog. The quick brown fox jumps over a
+lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps
+over a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox
+jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick
+brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The
+quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy
+dog. The quick brown fox jumps over a lazy dog. The quick brown fox jumps over
+a lazy dog. The quick brown fox jumps over a lazy dog. The quick brown fox
+jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The quick
+brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy dog. The
+quick brown fox jumps over a lazy dog. The quick brown fox jumps over a lazy
+dog.", formattedText);
+        }
+
+    }
+}
