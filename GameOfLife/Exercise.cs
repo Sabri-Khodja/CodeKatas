89818d285f91fd1100eda0258ead228bcabde920
Sabri Khodja <sabri.khodja@googlemail.com>
2024-05-15 13:16:19 +0200

    GameOfLife : initial state

---
+++ GameOfLife/Exercise.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameOfLife
+{
+    public static class Exercise
+    {
+        public static void GameOfLife(int[][] board) 
+        {
+            
+        }
+    }
+}
---
+++ GameOfLife/ExerciseUnitTests.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace GameOfLife
+{
+    [TestFixture]
+    public class ExerciseUnitTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+       
+    }
+}
